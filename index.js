1ec5f512a076ced5c1906a768bb5d73695f890c2
Bergkley <bergkley@gmail.com>
2024-09-22 23:09:47 -0300

    feat:Projeto-20-Memories-backend

--- index.js
+++ index.js
@@ -14,6 +14,11 @@ const memoryRoutes = require("./routes");
 
 app.use("/memories", memoryRoutes);
 
+app.get('/', (req, res) => {
+    return res.json("Hello word")
+})
+
+
 app.listen(port, () => {
     console.log(`Servidor online na porta ${port}`);
 })
